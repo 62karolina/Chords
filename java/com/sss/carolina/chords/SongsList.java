ad100596ffb70f32b977b5847ecdc877f69f7c0f
karolina <62karolina@mail.ru>
2017-08-23 00:00:00 +0300

    parsing

--- java/com/sss/carolina/chords/SongsList.java
+++ java/com/sss/carolina/chords/SongsList.java
@@ -1,15 +1,12 @@
 package com.sss.carolina.chords;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import org.jsoup.*;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -17,7 +14,8 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class SongsList extends AppCompatActivity {
+
 
     // благодоря этому классу мы будет разбирать данные на куски
     public Elements title;
@@ -31,47 +29,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_songs_list);
         // определение данных
-        lv = (ListView) findViewById(R.id.lvMain);
+        lv = (ListView) findViewById(R.id.lvSongList);
         // запрос к нашему отдельному поток на выборку данных
         new NewThread().execute();
         // Добавляем данные для ListView
         adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.product_name, titleList);
 
-        lv.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                class NewThread extends AsyncTask<String, Void, String> {
 
-                    Elements relHref;
-
-
-                    @Override
-                    protected String doInBackground(String... params) {
-                        try {
-                            doc = Jsoup.connect("http://5lad.ru").get();
-                            title = doc.select(".level1");
-                            relHref = title.select(".href");
-                            Intent intent = new Intent(MainActivity.this, SongsList.class);
-                            intent.putExtra("keyName", relHref);
-                            startActivity(intent);
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        return null;
-
-                    }
-                }
-
-            }
-
-        });
     }
-
 
     public class NewThread extends AsyncTask<String, Void, String> {
 
@@ -83,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
             // класс который захватывает страницу
             try {
 
-                doc = Jsoup.connect("http://5lad.ru").get();
-                title = doc.select(".level1");
+                doc = Jsoup.connect("http://5lad.ru" + getIntent().getStringExtra("keyName")).get();
+                title = doc.select(".level2");
                 String relHref = title.attr("href");
                 titleList.clear();
 
@@ -103,10 +71,11 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
 
             // после запроса обновляем листвью
-            lv.setAdapter(adapter);
+           lv.setAdapter(adapter);
         }
 
 
     }
-}
 
+
+}
